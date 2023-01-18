01f6d4acf4e8f72fe4826447cfdc5e0cc56fc618
Claudiu <stanclaudiu321@gmail.com>
2023-01-18 10:42:48 -0800

    Does the job, for a moment we got a good error

--- Models/RNModels/rn_model.py
+++ Models/RNModels/rn_model.py
@@ -2,7 +2,7 @@ import keras.models
 import numpy as np
 from Models.generic_ml_model import GenericMLModel
 from keras import backend
-from keras.layers import Dense, Input
+from keras.layers import Dense, Input, Dropout
 from keras.models import Model
 from keras.optimizers import Adam
 
@@ -11,15 +11,17 @@ class RNModel(GenericMLModel):
     def __init__(self, rn_model: Model = None):
         super().__init__()
         self.rn_model = rn_model
-        self.learning_rate = 0.00005
+        self.learning_rate = 0.0001
 
     def init_for_training(self, input_shape: int) -> None:
-        input_layer = Input(shape=input_shape)
+        input_layer = Input(shape=input_shape) ## 88
 
-        dense_layer_1 = Dense(256, kernel_initializer='lecun_normal', activation="selu")(input_layer)
-        dense_layer_2 = Dense(128, kernel_initializer='lecun_normal', activation="selu")(dense_layer_1)
+        dense_layer_1 = Dense(1024, kernel_initializer='lecun_normal', activation="selu",bias_initializer='zeros')(input_layer)
+        dropout_layer_1 = Dropout(0.2)(dense_layer_1)
+        dense_layer_2 = Dense(512, kernel_initializer='lecun_normal', activation="selu",bias_initializer='zeros')(dropout_layer_1)
+        dropout_layer_2 = Dropout(0.2)(dense_layer_2)
 
-        output_layer = Dense(1, activation="linear")(dense_layer_2)
+        output_layer = Dense(1)(dropout_layer_2)
 
         self.rn_model = Model(input_layer, output_layer)
         self.rn_model.compile(optimizer=Adam(learning_rate=self.learning_rate), loss="mean_squared_error")
